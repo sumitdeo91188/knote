530b8bb2271024cb64781da5e0b86a4cdc188945
sumitdeo91188 <sumitdeo1988@gmail.com>
2023-01-05 12:50:29 -0800

    Initial Commit

---
+++ src/main/java/com/deosu/knote/KnoteApplication.java
@@ -0,0 +1,13 @@
+package com.deosu.knote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deosu/knote/KnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deosu.knote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
